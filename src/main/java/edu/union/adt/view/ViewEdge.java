5e10859cfb32b791e096bbd481f3ff5ed9dda3be
Gamze Inanc <gamzeinanc@Gamzes-MacBook-Air.local>
2018-02-13 22:10:27 -0500

    add View Node and View Egde

---
+++ src/main/java/edu/union/adt/view/ViewEdge.java
@@ -0,0 +1,11 @@
+package edu.union.adt.fsm;
+
+
+public class ViewEdge
+    
+{
+    boolean[] selected = new boolean[]; //default false?
+    
+    Edge edge = new createEdge(); // String, node.getX, node.getY
+
+}
---
+++ src/main/java/edu/union/adt/view/ViewNode.java
@@ -0,0 +1,12 @@
+package edu.union.adt.fsm;
+
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+
+public class ViewNode
+{
+    boolean[] selected = new boolean[]; //default false?
+    Node node = new createNode(); //String, boolean
+    Ellipse.Double circle = new Ellipse2D.Double() // int int int int
+
+}
