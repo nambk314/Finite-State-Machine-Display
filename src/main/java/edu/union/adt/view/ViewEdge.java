536eb0fbca5d88560bcae2005ffeeed97e4d0d1b
Gamze Inanc <gamzeinanc@Gamzes-MacBook-Air.local>
2018-02-13 23:34:21 -0500

    View edge is changed

--- src/main/java/edu/union/adt/view/ViewEdge.java
+++ src/main/java/edu/union/adt/view/ViewEdge.java
@@ -1,16 +1,23 @@
-package edu.union.adt.view;
-
+package edu.union.adt.fsm;
 import java.awt.geom.Line2D;
 import edu.union.adt.fsm.Edge;
 
-public class ViewEdge
+public class ViewEdge {
+
+    Edge edge;
+    Line2D.Double line;
+
     
-{
-    boolean selected;; //default false?
-    
-    Edge edge; // String, node.getX, node.getY
-    public ViewEdge () {
-    	selected = false;
-    	
+    public  ViewEdge(int x1, int y1, int x2, int y2, Edge newEdge) {
+	edge = new Line2D.Double;
+	selected = false;
+	edge = newEdge;
     }
+
+    public Line2D returnEdge() {
+	return edge;
+    }
+    
 }
+
+
